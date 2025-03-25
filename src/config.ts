58b0cdb1695e44aa743cb27615eb5b9e819d77db
xiguaAp6y3 <100644212+xiguaAp6y3@users.noreply.github.com>
2025-03-25 13:33:29 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'XiGuaAp6y3',
-  subtitle: 'Gua3's Blog',
+  subtitle: 'Blog',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
