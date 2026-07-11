df8e2c5c034ef5a324cdd3a84582b7b2774510aa
Gua3 <2812652516@qq.com>
2026-07-11 21:27:28 +0800

    神秘界面

--- src/config.ts
+++ src/config.ts
@@ -43,6 +43,10 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.Archive,
     LinkPreset.About,
     {
+      name: '情人节惊喜',
+      url: '/confession/',
+    },
+    {
       name: 'GitHub',
       url: 'https://github.com/xiguaAp6y3',     // Internal links should not include the base path, as it is automatically added
       external: true,                               // Show an external link icon and will open in a new tab
